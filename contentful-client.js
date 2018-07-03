8344811b1bed8148d8f76b5045a69044deb28828
Alex Scott <williamascott@cedarville.edu>
2018-07-03 18:16:30 -0400

    adding comment

--- contentful-client.js
+++ contentful-client.js
@@ -1,6 +1,7 @@
 require('dotenv').load();
 const contentful = require('contentful');
 
+// initialize contenful client.
 module.exports = contentful.createClient({
   space: process.env.SPACE_ID,
   environment: 'master',
